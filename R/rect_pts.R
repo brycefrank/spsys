4113de73f64efc83935c8f226341cb852fb85565
brycefrank <bryce.frank@oregonstate.edu>
2020-07-16 15:38:23 -0700

    adding vignette, rect_pts documentation

---
+++ R/rect_pts.R
@@ -0,0 +1,12 @@
+#' Simulated data arranged in a rectangular grid.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A SpatialPointsDataFrame with [] and [] attributes.
+#' \describe{
+#'   \item{z_1}{The first attribute.}
+#'   \item{z_2}{The second attribute.}
+#'   ...
+#' }
+"rect_pts"
